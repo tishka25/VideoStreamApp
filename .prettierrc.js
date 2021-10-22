c1ff865483ba9dc1d180bdcf87d7b8eb21a42359
tishka25 <teodorstanishev@gmail.com>
2021-10-22 16:29:29 +0300

    temporary disable linters

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
-module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-};
+// module.exports = {
+//   bracketSpacing: false,
+//   jsxBracketSameLine: true,
+//   singleQuote: true,
+//   trailingComma: 'all',
+// };
+module.exports = {};
