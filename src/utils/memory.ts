47479cc227ae0e2eb7b832c541d19d273317862a
tishka25 <teodorstanishev@gmail.com>
2021-11-08 15:27:21 +0200

    add memory management

---
+++ src/utils/memory.ts
@@ -0,0 +1,43 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+export default new class Memory {
+    constructor(){
+
+    }
+
+    async save(key: string, data: Object){
+        AsyncStorage.setItem(key, JSON.stringify(data));
+    }
+
+    async load(key: string){
+        try {
+            const str = await AsyncStorage.getItem(key);
+            if(!str)
+                throw "Error";
+            return JSON.parse(str);
+        } catch (error) {
+            console.error("Could not parse key:", key);
+            return null;
+        }
+    }
+
+    async clear(key: string){
+        try {
+            return await AsyncStorage.removeItem(key);
+        } catch (error) {
+            console.error("Could not parse delete:", key);
+            return null;
+        }
+    }
+
+    async clearAll(){
+        try {
+            return await AsyncStorage.clear();
+        } catch (error) {
+            console.error("Could not clear local storage", error);
+            return null;
+        }
+    }
+
+}();
