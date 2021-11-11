c2d6591830f3856a53f68740ab449ef4f11f4620
tishka25 <teodorstanishev@gmail.com>
2021-11-11 20:50:19 +0200

    flex grid on recording tab page

--- src/screens/Recordings.tsx
+++ src/screens/Recordings.tsx
@@ -1,32 +1,79 @@
 
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Image, StyleSheet, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Title from '../components/Title';
 import { RootTabParamList } from '../types';
 import * as RootNavigation from "../rootNavigation";
+import channels, { LiveChannelItem } from '../utils/channels';
+import LoadingIndicator from '../components/LoadingIndicator';
+import { BASE_URL } from '../utils/constants';
 
 type Props = NativeStackNavigationProp<RootTabParamList, 'Recordings'>;
 
 export default function Recordings(props: Props) {
 
+    const [recordedChannels, setChannels] = useState<LiveChannelItem[]>([]);
+
+    async function loadData(){
+        setChannels(await channels.getRecordedChannels());
+    }
+
+    useEffect(()=>{
+        loadData();
+    }, []);
+
+    function renderChannels(){
+        return(
+            recordedChannels.map(channel=>{
+                return (
+                    <View style={styles.listItem}>
+                        <Image 
+                            source={{ uri: `${BASE_URL}${channel.logo}` }} 
+                            resizeMode="contain" 
+                            style={styles.channelListItemLogo} 
+                        />
+                    </View>
+                );
+            })
+        );
+    }
+
     return (
         <View style={styles.container}>
             <SafeAreaView />
+            <Title name="Записи за канали" />
             <ScrollView style={styles.container}>
-                <Title name="Записи за канали" />
+                <View style={styles.innerContainer}>
+                    {recordedChannels.length > 0 ? renderChannels() : <LoadingIndicator />}
+                </View>
             </ScrollView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "black",
-        width: "100%"
+    container:{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "black"
     },
+    scrollContainer: {
+        display: "flex",
+        backgroundColor: "black",
+        width: "100%",
+    },
+    innerContainer: {
+        flex: 1, 
+        flexDirection: 'row', 
+        flexWrap: 'wrap'
+    },
+    channelListItemLogo: { width: undefined, height: 80, aspectRatio: 1, marginTop: 48, marginHorizontal: 8},
+    listItem: {
+        // flex:1,
+        width:"33%"
+    }
 
 });
