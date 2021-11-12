de9629c14b318cbf5cd4fab3b8c01e74365a35ed
tishka25 <teodorstanishev@gmail.com>
2021-11-12 11:46:39 +0200

    add signout functionality

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import TvizioButton from '../components/TvizioButton';
 import user from '../utils/user';
-
+import * as RootNavigation from "../rootNavigation";
 
 export default function Profile() {
 
@@ -9,13 +10,21 @@ export default function Profile() {
 
   function renderProfileInfo(){
     return (
-      <Text style={styles.text}>{`Profile: ${userInfo.email}`}</Text>
+      <Text style={[styles.text, styles.child]}>{`Profile: ${userInfo.email}`}</Text>
     )
+  }
+
+  async function handleSignout(){
+    await user.signOut();
+    RootNavigation.reset("Login");
   }
 
   return (
     <View style={styles.container}>
       {renderProfileInfo()}
+      <View style={[styles.child]}>
+        <TvizioButton name="Излез" onPress={handleSignout}/>
+      </View>
     </View>
   );
 }
@@ -23,12 +32,15 @@ export default function Profile() {
 const styles = StyleSheet.create({ 
   container: {
     flex: 1,
-    backgroundColor: '#15152d',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
     color:"white",
   },
   text: {
     color: "white"
+  },
+  child: {
+    marginVertical: 16,
   }
 });
